a882c69a8c7e83c4490941a38630921c692c43b9
Zeigam <zeygammehtiev@gmail.com>
2024-08-28 04:35:31 +0400

    mobile first almost done

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const collection = document.querySelectorAll('.currencySelector');
+let curLeft;
+let curRight;
+
 for (let i = 0; i < collection.length; i++) {
     collection[i].onclick = function () {
-        alert(collection[i].classList.contains('selectedLeft'))
-        // const prev = document.querySelector('')
-        // collection[i].classList.add('')
+        if(collection[i].classList.contains('left')) {
+            if (!collection[i].classList.contains('selected')) {
+                sessionStorage.setItem('currencySelectedLeft', collection[i].classList.id);
+                document.querySelector('.left.selected').classList.remove('selected');
+                collection[i].classList.add('selected');
+            }
+        }
+        if(collection[i].classList.contains('right')) {
+            if (!collection[i].classList.contains('selected')) {
+                document.querySelector('.right.selected').classList.remove('selected');                collection[i].classList.add('selected');
+            }
+        }
     }
 };
