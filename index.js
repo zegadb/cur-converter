6e11da633dd6e09ccd9d24f399e1f9c853d0ce34
Zeigam <zeygammehtiev@gmail.com>
2024-08-31 03:56:08 +0400

    a bit more animations to header

--- index.js
+++ index.js
@@ -11,19 +11,27 @@ for (let i = 0; i < collection.length; i++) {
     selectorAnimation(i);
 }
 
+setTimeout(() => {
+    document.querySelector('#header').style.width = '660px';
+}, 1500);
 const welcomeMsg = document.querySelector('#welcomeMsgHeader');
+const welcomeMsgSmall = document.querySelector('#welcomeMsgHeaderSmall');
 // HEADER ANIMATION
 setTimeout(() => {
-    welcomeMsg.style.opacity = '100%';
+    welcomeMsg.style.opacity = '100%';   
 }, 1000);
 setTimeout(() => {
-    document.querySelector('#welcomeMsgHeaderSmall').style.opacity = '100%';
-}, 1200);
+    welcomeMsgSmall.style.opacity = '100%';
+    welcomeMsgSmall.style.transform = 'translate(0, 0)';
+}, 2400);
 
 const welcomeMsgText = 'Currency Converter';
 for (let i = 0; i < welcomeMsgText.length; i++) {
     setTimeout(() => {
         welcomeMsg.innerHTML += welcomeMsgText[i];
+        if (i == (welcomeMsgText.length - 1)) {
+            welcomeMsg.innerHTML += '&nbsp';
+        }
     }, 1000+i*100);
 }
 
