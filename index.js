f6ab3df1fc8ed7d8f91e75c870f3d52875d7261d
Zeigam <zeygammehtiev@gmail.com>
2024-08-31 02:07:07 +0400

    added animations to selectors

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const collection = document.querySelectorAll('.currencySelector');
 let curLeft;
 let curRight;
 
+function selectorAnimation (i) {
+    setTimeout(function () {
+        collection[i].style.opacity = "100%";
+    }, 1000+i*100);
+}
+for (let i = 0; i < collection.length; i++) {
+    selectorAnimation(i);
+}
+
+
 for (let i = 0; i < collection.length; i++) {
     collection[i].onclick = function () {
         if(collection[i].classList.contains('left')) {
